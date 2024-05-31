2f925284dc6a8b5abd8b7aaca277ac299c590bd3
Marc Lou <marc.louvion@gmail.com>
2024-05-31 15:07:41 +0800

    updated the hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main>
-			<button className="btn">Start now</button>
+			<h1>Collect customer feedback to build better products</h1>
+			<div>
+				Create a feeedback board in minutes, prioritize features, and
+				build products your customers will love.
+			</div>
 		</main>
 	);
 }
