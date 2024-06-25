74bb477c561e8b7208a7c4dcf0def2ed036661b1
Marc Lou <marc.louvion@gmail.com>
2024-06-25 13:47:53 +0800

    pre deployement

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import ButtonLogin from "@/components/ButtonLogin";
 import FAQListItem from "@/components/FAQListItem";
+import Image from "next/image";
+import productDemo from "./productDemo.jpeg";
 
 export default function Home() {
 	const isLoggedIn = true;
@@ -9,7 +11,7 @@ export default function Home() {
 		<main>
 			{/* HEADER */}
 			<section className="bg-base-200">
-				<div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+				<div className="max-w-5xl mx-auto flex justify-between items-center px-8 py-2">
 					<div className="font-bold">CodeFastSaaS</div>
 					<div className="space-x-4 max-md:hidden">
 						<a className="link link-hover" href="#pricing">
@@ -26,15 +28,23 @@ export default function Home() {
 			</section>
 
 			{/* HERO */}
-			<section className="text-center py-32 px-8 max-w-3xl mx-auto">
-				<h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
-					Collect customer feedback to build better products
-				</h1>
-				<div className="opacity-90 mb-10">
-					Create a feeedback board in minutes, prioritize features,
-					and build products your customers will love.
+			<section className="text-center lg:text-left py-32 px-8 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+				<Image
+					src={productDemo}
+					alt="Product demo"
+					className="w-96 rounded-xl"
+				/>
+
+				<div>
+					<h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
+						Collect customer feedback to build better products
+					</h1>
+					<div className="opacity-90 mb-10">
+						Create a feeedback board in minutes, prioritize
+						features, and build products your customers will love.
+					</div>
+					<ButtonLogin isLoggedIn={isLoggedIn} name={name} />
 				</div>
-				<ButtonLogin isLoggedIn={isLoggedIn} name={name} />
 			</section>
 
 			{/* PRICING */}
@@ -47,7 +57,7 @@ export default function Home() {
 						A pricing that adapts to your needs
 					</h2>
 
-					<div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto space-y-6">
+					<div className="p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-6">
 						<div className="flex gap-2 items-baseline">
 							<div className="text-4xl font-black">$19</div>
 							<div className="uppercase text-sm font-medium opacity-60">
